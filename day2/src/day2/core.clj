d5c819bf3a8ad5e46e16de972c14ac5a5ff399c0
virail <xjbviralx@gmail.com>
2024-12-03 09:43:22 +0000

    Completed first half of day 2, removing unnecessary files

--- day2/src/day2/core.clj
+++ day2/src/day2/core.clj
@@ -2,28 +2,45 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-(def reports (slurp "resources/reports.txt"))
-
 ;; take a line and process it
-(defn process-line [line]
-    (map #(Integer/parseInt %) bits))
-
-;; Saw Partition but that won't work
-
-(defn check
+(defn process-line
   [line]
-  (loop [cur (first line)
-         sec (second line)]
-    (if (> cur sec)
-      (if (> (- cur sec) 3)
-        false
-        (recur (rest line) sec))))
-    (if (< cur sec)
-      (if (> (- sec cur) 3)
-        false
-        (recur (rest line) sec))))
+  (let [split-line (str/split line #" ")]
+    (mapv #(Integer/parseInt %) split-line)))
+
+;; Saw Partition but that won't work (03/12/24 It Will WORK 😭)
+(defn direction?
+  [x y]
+  (cond
+    (< x y) "inc"
+    (> x y) "dec" 
+    :else "eq"))
+
+;; Check pair depending on if incrementing or decrementing
+(defn check-pair
+  [x y direction]
+  (cond
+    (= direction "inc") (and (< x y) (<= (- y x) 3))
+    (= direction "dec") (and (> x y) (<= (- x y) 3))
+    :else false))
+
+;; figure out how to process a line at a time
+(defn check-line
+  [line]
+  (let [current (process-line line)
+        direction (direction? (first current) (second current))
+        pairs (partition 2 1 current)]
+        (every? #(check-pair (first %) (second %) direction) pairs)))
+
+;; if decrementing then we need to make sure its within bounds of 1 <= dec <= 3
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [reports (slurp "resources/reports.txt")
+        split-reports (str/split-lines reports)
+        valid-lines (filter check-line split-reports)]
+    (println "Number of valid lines:" (count valid-lines))))
+
+;; Now we have to check and see if removing a singular element in a line will allow it to pass
+;; We can do this by removing an element and checking if the line is valid
